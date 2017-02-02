538154b8e948fdc380cd9c8c217f5d26c480580b
Derek Yu <yuderekyu@gmail.com>
2017-02-01 22:16:21 -0600

    Add skeleton for covenant gateway

---
+++ gateways/covenant.go
@@ -0,0 +1,16 @@
+package gateways
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/pborman/uuid"
+
+	"github.com/yuderekyu/covenant/config"
+	"github.com/yuderekyu/covenant/models"
+)
+
+/*Covenant wraps all methods of the covenant API*/
+type Covenant interface {
+
+}
